027efbd61fa1c4ed88b43adba4ea72556010c5e6
Raven Khaya <raven@stellar.tech>
2025-09-04 14:05:21 -0500

    changed /crowdstrike/ return

--- lib/match.go
+++ lib/match.go
@@ -232,7 +232,7 @@ func MatchCrowdstrikeText(params []string) string {
 
 	// Dual (both)
 	if s.Dual {
-		matched = append(matched, constants.CS_IP4, constants.CS_IP6)
+		matched = append(matched, constants.CS_IP4, constants.CS_IP6, constants.CS_DOMAINS)
 	}
 
 	merged := constants.Merge(types.List{}, matched...)
